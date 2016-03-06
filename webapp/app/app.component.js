87d0353c036c9da6ccd0b150c047d870f9411336
Steven Mirabito <steven@stevenmirabito.com>
2016-03-06 04:31:52 -0500

    Restructured project

--- webapp/app/app.component.js
+++ webapp/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './wallprompt.component'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,22 +8,39 @@ System.register(['angular2/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_1, wallprompt_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (wallprompt_component_1_1) {
+                wallprompt_component_1 = wallprompt_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.title = 'EventWall';
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'eventwall',
-                        template: '<h1>My First Angular 2 App</h1>'
-                    }), 
+                        template: "\n        <h1>{{title}}</h1>\n        <a [routerLink]=\"['WallPrompt']\">Wall Prompt</a>\n        <router-outlet></router-outlet>\n    ",
+                        directives: [router_1.ROUTER_DIRECTIVES],
+                        providers: [router_1.ROUTER_PROVIDERS]
+                    }),
+                    router_1.RouteConfig([
+                        {
+                            path: '/wall',
+                            name: 'WallPrompt',
+                            component: wallprompt_component_1.WallPromptComponent,
+                            useAsDefault: true
+                        }
+                    ]), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
---
+++ webapp/app/wallprompt.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'wallprompt',
+    template: '<h2>Enter your wall id</h2>'
+})
+
+export class WallPromptComponent { }
