392da30c12f1b86544ce3347e22dd6011f840f6a
Steven Mirabito <steven@stevenmirabito.com>
2016-03-06 09:36:38 -0500

    Upload route

--- eventwall/__init__.py
+++ eventwall/__init__.py
@@ -3,14 +3,44 @@ EventWall
 Flask Application Package
 """
 
-from flask import Flask
+import time
+import hashlib
+from flask import Flask, request, redirect
+from azure.storage.blob import BlobService
+from config import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def default():
+    return redirect("http://api.eventwall.org/", code=302)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        wall = request.form['wallId']
+        uploaded_file = request.files['inputFile']
+
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            blob_service = BlobService(account_name=azure_account_name, account_key=azure_account_key)
+            blob_service.put_block_blob_from_bytes(
+                    wall,
+                    blob_filename(email, uploaded_file.filename),
+                    uploaded_file.read(),
+                    max_connections=5
+            )
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in allowed_extensions
+
+
+def blob_filename(email, filename):
+    return str(hashlib.sha256(email).hexdigest()) + str(time.time()) + '.' + filename.rsplit('.', 1)[1]
 
 
 if __name__ == '__main__':
--- eventwall/config.example.py
+++ eventwall/config.example.py
@@ -3,6 +3,9 @@ EventWall
 Application Configuration
 """
 
+# Allowed file extensions for upload
+allowed_extensions = ['jpg', 'jpeg', 'png', 'gif', 'tiff']
+
 # Azure Storage
 azure_account_name = ""
 azure_account_key = ""
