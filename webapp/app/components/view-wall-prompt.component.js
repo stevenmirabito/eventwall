a767d7ef4e500420c0b1f63ae532d08947b84176
Steven Mirabito <steven@stevenmirabito.com>
2016-03-06 08:51:53 -0500

    Component updates

--- webapp/app/components/view-wall-prompt.component.js
+++ webapp/app/components/view-wall-prompt.component.js
@@ -1,6 +1,4 @@
-System.register(['angular2/core', './wall-prompt.component'], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
+System.register(['angular2/core', './wall-prompt.component'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -33,7 +31,7 @@ System.register(['angular2/core', './wall-prompt.component'], function(exports_1
                     __metadata('design:paramtypes', [])
                 ], ViewWallPromptComponent);
                 return ViewWallPromptComponent;
-            }());
+            })();
             exports_1("ViewWallPromptComponent", ViewWallPromptComponent);
         }
     }
--- webapp/app/components/wall-prompt.component.js
+++ webapp/app/components/wall-prompt.component.js
@@ -1,6 +1,4 @@
-System.register(['angular2/core', './form.component'], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
+System.register(['angular2/core', './form.component'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -33,7 +31,7 @@ System.register(['angular2/core', './form.component'], function(exports_1, conte
                     __metadata('design:paramtypes', [])
                 ], WallPromptComponent);
                 return WallPromptComponent;
-            }());
+            })();
             exports_1("WallPromptComponent", WallPromptComponent);
         }
     }
